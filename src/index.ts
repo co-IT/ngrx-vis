03aefb93f4f140b5d171d5ca16289dd242103bf1
Gregor Woiwode <gregor.woiwode@gmail.com>
2020-03-20 20:42:17 +0100

    enable use of flatMap

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
-import { Project } from 'ts-morph'
+import { Project, SourceFile } from 'ts-morph'
+
+function lookIntoFile(file: SourceFile): void {
+  const actions = file.getVariableDeclarations().map(declaration => ({
+    filePath: file.getFilePath(),
+    name: declaration.getName(),
+    type: declaration.getType().getText(),
+    references: declaration
+      .findReferences()
+      .flatMap(referenceSymbol =>
+        referenceSymbol
+          .getReferences()
+          .map(reference => reference.getSourceFile().getFilePath())
+      )
+  }))
+
+  console.log(actions)
+}
 
 const project = new Project({ tsConfigFilePath: `./ngrx-app/tsconfig.json` })
 
-const files = project.getSourceFiles('**/*.ts')
+const files = project.getSourceFiles('**/*.actions.ts')
 
-console.log(files)
+files.forEach(file => lookIntoFile(file))
