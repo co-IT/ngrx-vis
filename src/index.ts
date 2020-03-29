b03c14faf07dba8d44c657c71c465a992f245350
Gregor Woiwode <gregor.woiwode@gmail.com>
2020-03-29 12:06:56 +0200

    print error when web-view-creation fails

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import { createNetwork } from './visjs/create-network'
-import { createWebView } from './web-view/create-web-view'
 import { NgRxActionInspector } from './core/ngrx-action-inspector'
+import { createNetwork } from './vis-js/create-network'
+import { createWebView } from './web-view/create-web-view'
 
 const inspector = new NgRxActionInspector('./ngrx-example-app/tsconfig.json')
 const network = createNetwork(inspector.inspect())
 
-createWebView(network)
+// eslint-disable-next-line no-console
+createWebView(network).catch(err => console.log(err))
--- src/web-view/create-web-view.ts
+++ src/web-view/create-web-view.ts
@@ -1,17 +1,19 @@
-import { copySync, ensureDir, readFileSync, writeFileSync } from 'fs-extra'
-import { DataSet } from '../visjs'
+import { copy, ensureDir, readFile, writeFile } from 'fs-extra'
 
-export function createWebView(dataSet: DataSet) {
-  ensureDir('./ngrx-vis')
-  copySync('./src/web-view/template', './ngrx-vis/')
-
-  const graphJsFile = readFileSync('./ngrx-vis/src/network-graph.js', {
-    encoding: 'utf-8'
-  })
-
-  const filledGraphJsFile = graphJsFile
-    .replace('/* __NETWORK_NODES__ */', JSON.stringify(dataSet.nodes))
-    .replace('/* __NETWORK_EDGES__ */', JSON.stringify(dataSet.edges))
-
-  writeFileSync('./ngrx-vis/src/network-graph.js', filledGraphJsFile)
+export function createWebView(dataSet: { nodes: any[]; edges: any[] }) {
+  return ensureDir('./ngrx-vis')
+    .then(() => copy('./src/web-view/template', './ngrx-vis/'))
+    .then(() =>
+      readFile('./ngrx-vis/src/network-graph.js', {
+        encoding: 'utf-8'
+      })
+    )
+    .then(graphJsFile =>
+      graphJsFile
+        .replace('/* __NETWORK_NODES__ */', JSON.stringify(dataSet.nodes))
+        .replace('/* __NETWORK_EDGES__ */', JSON.stringify(dataSet.edges))
+    )
+    .then(filledGraphJsFile =>
+      writeFile('./ngrx-vis/src/network-graph.js', filledGraphJsFile)
+    )
 }
